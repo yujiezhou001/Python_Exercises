f469a36b6855e100e6bf838adae94c8931a2e6ba
Gordon <gordonzhou68@outlook.com>
2019-07-27 00:39:06 +0000

    code update

--- ex7.py
+++ ex7.py
@@ -2,46 +2,45 @@ import random
 from urllib.request import urlopen
 import sys
 
-WORD_URL = "http://learncodethehardway.org/words.txt" 
+WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
     "class %%%(%%%):":
       "Make a class named %%% that is-a %%%.",
     "class %%%(object):\n\tdef __init__(self, ***)" :
-      "class %%% has-a __init__ that takes self and *** params.", 
+      "class %%% has-a __init__ that takes self and *** parameters.",
     "class %%%(object):\n\tdef ***(self, @@@)":
-      "class %%% has-a function *** that takes self and @@@ params.",
+      "class %%% has-a function named *** that takes self and @@@ parameters.",
     "*** = %%%()":
       "Set *** to an instance of class %%%.",
     "***.***(@@@)":
-      "From *** get the *** function, call it with params self, @@@.",
+      "From *** get the *** function, and call it with parameters self, @@@.",
     "***.*** = '***'":
       "From *** get the *** attribute and set it to '***'."
 }
 
 # do they want to drill phrases first
 if len(sys.argv) == 2 and sys.argv[1] == "english":
-     PHRASE_FIRST = True
+    PHRASE_FIRST = True
 else:
     PHRASE_FIRST = False
 
 # load up the words from the website
-for word in urlopen(WORD_URL).readlines(): 
+for word in urlopen(WORD_URL).readlines():
     WORDS.append(str(word.strip(), encoding="utf-8"))
 
 
 def convert(snippet, phrase):
     class_names = [w.capitalize() for w in
-            random.sample(WORDS, snippet.count("%%%"))]
+                   random.sample(WORDS, snippet.count("%%%"))]
     other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
 
     for i in range(0, snippet.count("@@@")):
         param_count = random.randint(1,3)
-        param_names.append(', '.join(
-            random.sample(WORDS, param_count)))
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
         result = sentence[:]
@@ -62,20 +61,22 @@ def convert(snippet, phrase):
 
     return results
 
-# keep going until
+
+# keep going until they hit CTRL-D
 try:
     while True:
         snippets = list(PHRASES.keys())
         random.shuffle(snippets)
 
-    for snippet in snippets:
-        phrase = PHRASES[snippet]
-        question, answer = convert(snippet, phrase)
-        if PHRASE_FIRST:
-            question, answer = answer, question
-            
-        print(question)
-        input("> ")
-        print(f"ANSWER:  {answer}\n\n")
+        for snippet in snippets:
+            phrase = PHRASES[snippet]
+            question, answer = convert(snippet, phrase)
+            if PHRASE_FIRST:
+                question, answer = answer, question
+
+            print(question)
+
+            input("> ")
+            print(f"ANSWER:  {answer}\n\n")
 except EOFError:
     print("\nBye")
