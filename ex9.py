81459c515610c1c079f438a7ec66b2276e52e564
Gordon <gordonzhou68@outlook.com>
2019-08-02 17:45:37 -0400

    game engine

---
+++ ex9.py
@@ -0,0 +1,37 @@
+class Scene(object):
+
+    def enter(self):
+        pass
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+class Death(Scene):
+
+    def enter(self):
+        pass
+
+class CentralCorridor(Scene):
+
+    def enter(self):
+        pass
+
+class LaserWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+class EscapePod(Scene):
+    def enter(self):
+        pass
+        
