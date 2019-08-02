9979bff567aabeade7e8517025f07eca1cb1d9f2
Gordon <gordonzhou68@outlook.com>
2019-08-02 17:47:28 -0400

    game engine continues

--- ex9.py
+++ ex9.py
@@ -32,6 +32,21 @@ class TheBridge(Scene):
         pass
 
 class EscapePod(Scene):
+
     def enter(self):
         pass
-        
+
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
