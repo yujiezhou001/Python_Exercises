5e664b662c5e39604f8bb62b5de94fe199cdcd16
Gordon <gordonzhou68@outlook.com>
2019-09-25 23:31:39 -0400

    add comments on objects and classes of game in ex9.py the goal is to be familiar with object oriented programming

--- ex9.py
+++ ex9.py
@@ -7,7 +7,7 @@
 #   The Bridge
 #   Escape Pod
 
-
+# The structure of the game
 
 # Map
 #     next_scene
@@ -21,11 +21,12 @@
 #     Laser Weapon Armory * The Bridge
 #     Escape Pod
 
+#Scene is an object
 class Scene(object):
 
     def enter(self):
         pass
-
+#Engine is an object
 class Engine(object):
 
     def __init__(self, scene_map):
@@ -33,32 +34,32 @@ class Engine(object):
 
     def play(self):
         pass
-
+#Death is an instance of Scene object
 class Death(Scene):
 
     def enter(self):
         pass
-
+#CentralCorridor is an instance of Scene object
 class CentralCorridor(Scene):
 
     def enter(self):
         pass
-
+#LaserWeaponArmory is an instance of Scene object
 class LaserWeaponArmory(Scene):
 
     def enter(self):
         pass
-
+#TheBridge is an instance of Scene object
 class TheBridge(Scene):
 
     def enter(self):
         pass
-
+#EscapePod is an instance of Scene object
 class EscapePod(Scene):
 
     def enter(self):
         pass
-
+#Map is an object
 class Map(object):
 
     def __init__(self, start_scene):
@@ -69,7 +70,9 @@ class Map(object):
 
     def opening_scene(self):
         pass
-
+#First create a map which selects a start_scene
 a_map = Map('central_corridor')
+#Then create a game with engine using the established map
 a_game = Engine(a_map)
+#Start the engine and run the game
 a_game.play()
